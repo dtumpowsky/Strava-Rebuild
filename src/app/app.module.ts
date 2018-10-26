89af63d1aa51a738d7820668dfab81e0100db845
David Tumpowsky <tumpowskydavid@gmail.com>
2018-10-26 10:29:55 -0700

    login page start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }  from '@angular/forms';
+// import { routing } from './app.routing';
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
+    // routing
   ],
   providers: [],
   bootstrap: [AppComponent]
